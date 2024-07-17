a7c2c449c05e19e140cf8e018f736c99ebf7cac5
Jay-Ubisse <joaquimubisse@gmail.com>
2024-07-17 15:58:43 +0200

    feat: add intro to express lesson

---
+++ express/introducao/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const port = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.type("text/html");
+  res.send("<h1>Hello Express</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.type("text/html");
+  res.send(`<h1>Olá, ${req.query.name}</h1>`);
+});
+
+app.use((req, res) => {
+  res.type("text/html");
+  res.status(404);
+  res.send("<h1>Página não encontrada.</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`➡️ Local: http://localhost:${port}`);
+});
